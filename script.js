187285e50e30d0e7d3031e926561bd63164496a8
Joseph Ayman <josephayman@mail.com>
2022-10-13 16:51:24 +0200

    Day 6

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(".form-close-button").click(function() {
 });
 
 // Add a new contact to the table
-$("#add-contact-button").click(function() {
+/* $("#add-contact-button").click(function() {
     const name = $("#name").val();
     const surname = $("#surname").val();
     const number = $("#number").val();
@@ -42,7 +42,33 @@ $("#add-contact-button").click(function() {
 
     $("#contacts-table").prepend(newContact);
     $(".form-wrapper").css("display", "none");
+}); */
+
+// If the new contact already exists, show an error message
+$("#add-contact-button").click(function() {
+    const name = $("#name").val();
+    const surname = $("#surname").val();
+    const number = $("#number").val();
+    const address = $("#address").val();
+    const newContact = `
+        <tr>
+            <td> <div class="icon">${name[0]+surname[0]}</div> </td>
+            <td>${name}</td>
+            <td>${surname}</td>
+            <td>${number}</td>
+            <td>${address}</td>
+            <td> <button class="delete-button">X</button> </td>
+        </tr>
+    `;
+
+    if (name === "" || surname === "" || number === "" || address === "") {
+        $(".error-message").css("display", "block");
+    } else {
+        $("#contacts-table").prepend(newContact);
+        $(".form-wrapper").css("display", "none");
+    }
 });
+
 
 // Show the Delete modal
 $(document).on("click", ".delete-button", function() {
